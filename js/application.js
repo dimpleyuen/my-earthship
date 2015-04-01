14b59e6c2f5ff3cebae5475f280be5dab02ef4b7
Dimple Yuen <dimpleyuen@gmail.com>
2015-04-01 17:46:26 +0800

    wrote function for progress bar, relevant tips and results

--- js/application.js
+++ js/application.js
@@ -1,3 +1,5 @@
+var result = 0;
+
 $(document).ready(function() {
 
   $('.answers').hide();
@@ -35,24 +37,60 @@ $(document).ready(function() {
     showNextQuestion();
   })
 
-//On Button Click, store the input field somewhere
+//On Button Click, store the input field somewhere and put it in the span
   $('.start-quiz').click(function() {
-    var userName = $('.input-name').val();
+    var name = $('.input-name').val();
+    $('.username').text(name);
   })
 
-//accumulate points upon clicking different answers
-  var result = 0;
+//Accumulate Points upon clicking different answers
+  $(document).on('click', '.answer-wrapper', function() {
+    var score = Number($(this).attr('data-score'));
+    result = result + score;
+    calcScores();
+  })
+
+//Add Scores
+  function calcScores() { 
+    if ( result == 20 ) {
+      $('.results').text("Are Master Level");
+    }
+    if ( result == -10 || result == 0 || result == 10 ) {
+      $('.results').text("I Think You Can Do Better");
+    }
+    if ( result == -20 ) {
+      $('.results').text("C'mon, You Aren't Even Trying");
+
+    }
+  }
+
+//Hide All The Tips
+  $('.tips-wrapper').children().hide();
+
+//Show Relevant Tips On Results Page
+  $(document).on('click', '.answer-wrapper', function() {
+    if (  $(this).attr('data-score') <= 0  ) {
+      $($('.tips-wrapper').children()[currentAnswerIndex-2]).show();
+    }
+  })
+
+//Update Progress Bar
+  var progressVal = 1;
+  var progressWidth = 11.5;
+
+  $(document).on('click', '.answer-wrapper', function() {
+    progressVal ++;
+    progressWidth += 11.5;
+    var styleValue = "width: " + progressWidth + "%;";
+
+    $('.progress-bar').attr("aria-valuenow", progressVal);
+    $('.progress-bar').attr("style", styleValue);
+    $($('.progress-bar')[currentAnswerIndex-1]).text(currentAnswerIndex+"/9");
+  })
 
 
 
 
 
+})
 
-
-
-
-//accumulate whatever is the data-score into a variable called result
-
-
-
-//
